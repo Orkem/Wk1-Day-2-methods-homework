20b743f42dda86e46b223059cfc57f9762c9f5b2
Chris Hunter <orkem2@gmail.com>
2017-06-27 17:14:24 +0100

    Alerative full_month_name

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -30,35 +30,40 @@ def add_string_as_number(num1, num2)
   return num1.to_i + num2.to_i
 end
 
+# def number_to_full_month_name(month)
+#   case month
+#     when 1
+#       return "January"
+#     when 2
+#       return "February"
+#     when 3
+#       return "March"
+#     when 4
+#       return "April"
+#     when 5
+#       return "May"
+#     when 6
+#       return "June"
+#     when 7
+#       return "July"
+#     when 8
+#       return "August"
+#     when 9
+#       return "September"
+#     when 10
+#       return "October"
+#     when 11
+#       return "November"
+#     when 12
+#       return "December"
+#     else
+#       "Say what?!"
+#     end
+# end
+
 def number_to_full_month_name(month)
-  case month
-    when 1
-      return "January"
-    when 2
-      return "February"
-    when 3
-      return "March"
-    when 4
-      return "April"
-    when 5
-      return "May"
-    when 6
-      return "June"
-    when 7
-      return "July"
-    when 8
-      return "August"
-    when 9
-      return "September"
-    when 10
-      return "October"
-    when 11
-      return "November"
-    when 12
-      return "December"
-    else
-      "Say what?!"
-    end
+  arr = ["January", "February", "March", "April", "May", "June", "July", "August","September", "October", "November", "December"]
+  return arr[month - 1]
 end
 
 def number_to_short_month_name(month)
